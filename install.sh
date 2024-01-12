5299f0428a0b12fdba7a286a9bdcd779f89e9549
abmmhasan <abmmhasan@gmail.com>
2024-01-12 18:24:13 +0600

    shell update

--- install.sh
+++ install.sh
@@ -7,27 +7,33 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install Dependencies
+printf "Installing Dependencies\n"
 apt update
 apt upgrade -y
-apt install git curl unbound sudo dnsmasq  -y
+apt install git wget curl unbound sudo net-tools gamin sysv-rc-conf lighttpd lighttpd-mod-deflate -y
 
-echo "edns-packet-max=1232" >> /etc/dnsmasq.d/99-edns.conf
-chmod -R 644 /etc/dnsmasq.d/99-edns.conf
-cp configs/pihole.conf /etc/unbound/unbound.conf.d/
-chmod -R 644 /etc/unbound/unbound.conf.d/pihole.conf
+printf "Configuring Unbound\n"
+cp configs/pi-hole.conf /etc/unbound/unbound.conf.d/
+chmod -R 644 /etc/unbound/unbound.conf.d/pi-hole.conf
+service unbound-resolvconf stop
+sysv-rc-conf unbound-resolvconf off
+sed -Ei 's/^unbound_conf=/#unbound_conf=/' /etc/resolvconf.conf
+rm /etc/unbound/unbound.conf.d/resolvconf_resolvers.conf
+service unbound restart
 
-sudo service unbound-resolvconf stop
-sudo chkconfig unbound-resolvconf off
-sudo sed -Ei 's/^unbound_conf=/#unbound_conf=/' /etc/resolvconf.conf
-sudo rm /etc/unbound/unbound.conf.d/resolvconf_resolvers.conf
-sudo service unbound restart
-
+printf "Setup Pi-hole\n"
 mkdir -p /etc/pihole/
 cp configs/setupVars.conf /etc/pihole/
 chmod -R 644 /etc/pihole/setupVars.conf
 source /etc/pihole/setupVars.conf
-useradd -ms /bin/bash pi-serve && export USER=pi-serve
+useradd -ms /bin/bash pi-hole && export USER=pi-hole
 curl -sSL https://install.pi-hole.net | bash -sex -- --unattended
 
+printf "Configuring DNSMasq\n"
+echo "edns-packet-max=1232" >> /etc/dnsmasq.d/99-edns.conf
+chmod -R 644 /etc/dnsmasq.d/99-edns.conf
+/etc/init.d/dnsmasq restart
+service network-manager restart
+
 apt clean
-sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
